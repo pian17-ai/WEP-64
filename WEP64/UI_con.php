cd5a03762a6821d2e3a798fa9d420c604ef370f6
Alvian Cahyo Pambudi <alviancahyopambudi@gmail.com>
2025-08-24 14:08:45 +0700

    settings

--- WEP64/UI_con.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "mhs_ui";
-    $conn = new mysqli($host, $username, $password, $database);
-?>
--- WEP64/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-    $host = "localhost";
-    $user = "root";
-    $password = "";
-    $database = "dbWEP64";
-    $conn = new mysqli($host, $user, $password, $database);
-
-?>
